871d21daadb007f2946ea0178c532ec2b7bb27ac
Alejandro <lopalejandro@gmail.com>
2021-11-01 10:54:11 -0300

    Primer Commit

---
+++ Objetos/main.py
@@ -0,0 +1,11 @@
+import player
+
+sorcerer =player.Sorcerer(hit_point=40,mana=80)
+knight = player.Knight(mana=20,hit_point=80)
+paladin = player.Paladin()
+druid = player.Druid()
+
+print (sorcerer)
+print (knight)
+print (paladin)
+print (druid)
---
+++ Objetos/player.py
@@ -0,0 +1,46 @@
+class Player:
+
+    vocation = "No vocation"
+    spell = "Puff"
+    movement_speed = 50
+
+    def __init__(self,**kwargs) :
+        self.hit_point = kwargs.get('hit_point',50)
+        self.mana = kwargs.get('mana',50)
+        self.name = input("Elige tu nombre : ")
+
+    def __str__(self):
+        return "{} es un  {} tiene {} de vida y {} de mana , puede lanzar {} y corre a {} movement speed\n".format(
+                    self.name,
+                    self.vocation,
+                    self.hit_point,
+                    self.mana,
+                    self.cast_spell(),
+                    self.movement_speed)
+
+    def cast_spell(self):
+        return self.spell
+
+class Sorcerer(Player):
+
+    vocation = "Sorcerer"
+    spell = "Utevo Lux"
+    movement_speed = 20
+
+class Knight(Player):
+
+    vocation = "Knigth"
+    spell = "exordi"
+    movement_speed = 80
+
+class Paladin(Player):
+
+    vocation = "Paladin"
+    spell = "spider"
+    movement_speed = 50
+
+class Druid(Player):
+
+    vocation = "Druid"
+    spell = "Tree"
+    movement_speed = 20
